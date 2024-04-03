23c1aae721aa2421a6e21973c46a8cf132ce3469
petrkucerak <petr@khome.cz>
2024-04-03 20:22:55 +0200

    Add image ../../../tmp/foto/gorun/MG_00040.jpg

--- scripts/upload-images.sh
+++ scripts/upload-images.sh
@@ -39,8 +39,57 @@ for file in "$source_folder"/*; do
       yarn gallery
       yarn img2webp "$destination_folder/$file_name"
 
+      rm "$destination_folder/$file_name"
+
       # Commit changes
-      git add *
+      git add ../foto/*
+      git commit -m "Add image $file"
+      git pull
+      git push
+
+      echo -e "${YELLOW}Image has been commited${NC}"
+      echo ""
+   fi
+done
+
+echo "Copy process completed."
+
+# Source and destination folders
+source_folder_urban="../../../tmp/foto/urban"
+destination_folder_urban="../foto/urban"
+
+# Check if source folder exists
+if [ ! -d "$source_folder_urban" ]; then
+   echo "Source folder does not exist."
+   exit 1
+fi
+
+# Check if destination folder exists, if not, create it
+if [ ! -d "$destination_folder_urban" ]; then
+   mkdir -p "$destination_folder_urban"
+fi
+
+# Loop through each file in the source folder
+for file in "$source_folder_urban"/*; do
+   # Check if the item is a file
+   if [ -f "$file" ]; then
+      # Extract the file name from the full path
+      file_name=$(basename "$file")
+
+      # Copy the file to the destination folder
+      cp "$file" "$destination_folder_urban/$file_name"
+
+      echo -e "${GREEN}Copied: $file_name${NC}"
+
+      # Generate files
+      rm ../foto/urban/images.json
+      yarn gallery
+      yarn img2webp "$destination_folder_urban/$file_name"
+
+      rm "$destination_folder_urban/$file_name"
+
+      # Commit changes
+      git add ../foto/*
       git commit -m "Add image $file"
       git pull
       git push
