0f59d483dbead5627f5285d82f96118861197c36
petrkucerak <petr@khome.cz>
2024-04-03 20:12:24 +0200

    Add image ../../../tmp/foto/gorun/MG_00001.jpg

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -8,11 +8,11 @@ console.log(
   "Prepare JSON format for https://www.npmjs.com/package/react-photo-gallery-next"
 );
 
-const processImages = async (image_path) => {
+const processImages = async () => {
   let array = [];
 
   try {
-    const photos = fs.readdirSync(image_path);
+    const photos = fs.readdirSync(image_path_gorun);
 
     // Process images sequentially
     await Promise.all(
@@ -20,7 +20,7 @@ const processImages = async (image_path) => {
         let width, height;
 
         try {
-          const info = await sharp(`${image_path}/${p}`).metadata();
+          const info = await sharp(`${image_path_gorun}/${p}`).metadata();
 
           width = info.width > info.height ? 4 : 3;
           height = info.width > info.height ? 3 : 4;
@@ -41,7 +41,48 @@ const processImages = async (image_path) => {
     );
 
     // Write array into the file
-    const file = fs.createWriteStream(`${image_path}/images.json`);
+    const file = fs.createWriteStream(`${image_path_gorun}/images.json`);
+    file.write(JSON.stringify(array));
+    file.end();
+
+    console.log("Processing completed successfully.");
+  } catch (err) {
+    console.error("Error reading directory:", err);
+  }
+
+  array = [];
+
+  try {
+    const photos = fs.readdirSync(image_path_urban);
+
+    // Process images sequentially
+    await Promise.all(
+      photos.map(async (p) => {
+        let width, height;
+
+        try {
+          const info = await sharp(`${image_path_urban}/${p}`).metadata();
+
+          width = info.width > info.height ? 4 : 3;
+          height = info.width > info.height ? 3 : 4;
+
+          if (p.includes(".webp"))
+            array.push({
+              src: `https://foto2024.diecezko.cz/foto/gorun/${p}`,
+              original: `https://foto2024.diecezko.cz/foto/gorun/${p}`,
+              width: width,
+              height: height,
+              alt: "Fotka z jarního Diecézka 2024",
+              caption: "Fotka z jarního Diecézka 2024",
+            });
+        } catch (err) {
+          console.error(`Error processing image ${p}:`, err);
+        }
+      })
+    );
+
+    // Write array into the file
+    const file = fs.createWriteStream(`${image_path_urban}/images.json`);
     file.write(JSON.stringify(array));
     file.end();
 
@@ -52,5 +93,4 @@ const processImages = async (image_path) => {
 };
 
 // Call the function to start processing
-processImages(image_path_gorun);
-processImages(image_path_urban);
+processImages();
